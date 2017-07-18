ca80a59ddfa576e1a257b55912cfb861538a60fd
某M <mmnstar@hotmail.com>
2017-07-18 13:22:36 +0800

    20170718

---
+++ MCAB-PC/MCAB-PC/Forms/frmProgress.vb
@@ -0,0 +1,11 @@
+﻿Public Class frmProgress
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ProgressBar1.Maximum = 100
+        ProgressBar1.Minimum = 0
+        For L As Long = 0 To 100
+            Threading.Thread.Sleep(50)
+            Application.DoEvents()
+            ProgressBar1.Value = L
+        Next
+    End Sub
+End Class
