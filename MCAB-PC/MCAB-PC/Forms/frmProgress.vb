9b6abb7b5bda5fa454f456fce356c141144dcf82
某M <mmnstar@hotmail.com>
2017-08-01 16:08:49 +0800

    20170801

--- MCAB-PC/MCAB-PC/Forms/frmProgress.vb
+++ MCAB-PC/MCAB-PC/Forms/frmProgress.vb
@@ -1,5 +1,6 @@
 ﻿Public Class frmProgress
     Public frmParent As Form
+
     ''' <summary>
     ''' 显示进度条
     ''' </summary>
@@ -18,7 +19,7 @@
         Me.Show()
         If Label1.Text <> strS Then Label1.Text = strS
         ProgressBar1.Value = Value
-
+        sbSleepEx()
         Application.DoEvents()
     End Sub
     ''' <summary>
@@ -32,35 +33,72 @@
                                 ByVal DBName As String,
                                 ByVal UserName As String,
                                 ByVal Password As String)
-        Dim strConnect As String = "data source=" & ServerName & ";initial catalog=" &
-                                   ";user id=" & UserName & ";password=" &
-                                   Password & ";”
-        Dim sqlConnection1 As SqlClient.SqlConnection =
-            New System.Data.SqlClient.SqlConnection(strConnect)
-        'TODO
-        ''''''sqlConnection1.ConnectionString = strConnect
+        Dim L As Long = 1
+        Dim strConnect As String
+        Dim sqlConnection1 As New SqlClient.SqlConnection
+        'Dim sqlConnection1 As SqlClient.SqlConnection = New System.Data.SqlClient.SqlConnection(strConnect)
+        Dim sqlCmd As New SqlClient.SqlCommand
+        'Dim sqlCmd As SqlClient.SqlCommand = New SqlClient.SqlCommand(sql, sqlConnection1)
+
         Try
             '创建数据库
-            sbProg(1, 1, "正在创建数据库...")
-            Dim sql As String = "CREATE DATABASE [" & DBName & "]"
+            sbProg(1, 5, "正在创建数据库...")
+            strConnect = "data source=" & ServerName & ";initial catalog=" &
+                         ";user id=" & UserName & ";password=" & Password & ";”
+            sqlConnection1.ConnectionString = strConnect
             sqlConnection1.Open()
-            Dim sqlCmd As SqlClient.SqlCommand =
-                New SqlClient.SqlCommand(sql, sqlConnection1)
+            sqlCmd.Connection = sqlConnection1
+            sqlCmd.CommandText = "CREATE DATABASE [" & DBName & "]"
             sqlCmd.ExecuteNonQuery()
             sqlConnection1.Close()
 
-            '创建系统表 - 目录索引
+            sbProg(2)
             strConnect = "data source=" & ServerName & ";initial catalog=" & DBName &
-                         ";user id=" & UserName & ";password=" &
-                         Password & ";”
-            sql = "CREATE TABLE DirectoryIndex (id INTEGER CONSTRAINT PKeyMyId PRIMARY KEY,
-                   DirName CHAR(255))"
-            'sqlConnection1 = SqlClient.SqlCommand(sql, sqlConnection1)
+                         ";user id=" & UserName & ";password=" & Password & ";”
+            sqlConnection1.ConnectionString = strConnect
+            Do
+                L = 0
+                Try
+                    sqlConnection1.Open()
+                    L = MAX_SQL_RETRY + 1
+                Catch ex As SqlClient.SqlException
+                    If ex.Number = 4060 Then
+                        L = L + 1
+                    Else
+                        MsgBox(ex.ToString)
+                    End If
+                Catch ex As Exception
+                    MsgBox(ex.ToString)
+                End Try
 
+            Loop Until l >= MAX_SQL_RETRY
+            sqlCmd.Connection = sqlConnection1
 
+            '创建系统表 - 基本信息库
+            sbProg(3)
+            sqlCmd.CommandText = "CREATE TABLE BasicInfo (
+                                  id INTEGER CONSTRAINT PK_BasicInfo PRIMARY KEY,
+                                  Name NVARCHAR(50),
+                                  Value NVARCHAR(MAX))"
+            sqlCmd.ExecuteNonQuery()
+            sqlCmd.CommandText = "INSERT INTO BasicInfo VALUES ( '1','Title','档案管理系统')"
+            sqlCmd.ExecuteNonQuery()
+            sqlCmd.CommandText = "INSERT INTO BasicInfo VALUES ( '2','Version','1.0')"
+            sqlCmd.ExecuteNonQuery()
 
-            'Catch ex As System.Data.SqlClient.SqlException
-            '    MsgBox(ex.ToString)
+            '创建系统表 - 目录索引
+            sbProg(4)
+            sqlCmd.CommandText = "CREATE TABLE DirectoryIndex (
+                                  id INTEGER CONSTRAINT PK_DirectoryIndex PRIMARY KEY,
+                                  hierarchy INTEGER,
+                                  DirName CHAR(255))"
+            sqlCmd.ExecuteNonQuery()
+            sqlCmd.CommandText = "INSERT INTO DirectoryIndex VALUES ( '1','0','档案目录1')"
+            sqlCmd.ExecuteNonQuery()
+            sqlCmd.CommandText = "INSERT INTO DirectoryIndex VALUES ( '2','0','档案目录2')"
+            sqlCmd.ExecuteNonQuery()
+            sbProg(5)
+
         Catch ex As Exception
             MsgBox(ex.ToString)
         End Try
@@ -71,7 +109,7 @@
         Finally
         End Try
 
-        sbSleepEx(2000)
+        'sbSleepEx(2000)
         Me.Close()
     End Sub
 
--- MCAB-PC/MCAB-PC/Moudles/mdMACB.vb
+++ MCAB-PC/MCAB-PC/Moudles/mdMACB.vb
@@ -1,6 +1,10 @@
 ﻿Module mdMACB
+
+    Public Const MAX_SQL_RETRY As Long = 100 'SQL操作失败时尝试重复次数
+
+
     Private Const SLEEP_INTERVAL As Long = 50 'Sleep每次刷新时间间隔，单位ms
-    Public Sub sbSleepEx(ByVal ms As Long)
+    Public Sub sbSleepEx(Optional ByVal ms As Long = SLEEP_INTERVAL)
         For L As Long = ms To 0 Step -SLEEP_INTERVAL
             Threading.Thread.Sleep(SLEEP_INTERVAL)
             Application.DoEvents()
